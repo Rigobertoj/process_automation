3a5383ef1e0ee9213f77f66eddd7bcab80b0c52d
Rigoberto martinez <donojaci@gmail.com>
2022-12-29 09:47:58 -0600

    analisando los XML para asi decidir cual sera la estrucutra de la calse reed_xml que problablemente se renombre como reed_CFDI

---
+++ read_CFDI/reed_xml.py
@@ -0,0 +1,6 @@
+class reed_xml :
+    CFDI = {}
+    def __init__(self, document) -> None:
+        self.xml = document
+
+    # def 
--- read_CFDI/test_tree_xml.py
+++ read_CFDI/test_tree_xml.py
@@ -13,35 +13,49 @@ En este script lo que queremos es obtener una serie de valores
 - Total
 """
 
+# asi se importa la libreria de xml
 import xml.etree.cElementTree as ET
 
-tree = ET.parse("data.xml") 
+CFDI_TASA_0 = "./CFDI/7513B197-3F46-4807-B4E6-1001AAA07248.xml"
+CFDI_TASA_SIMPLE = "./CFDI/A219EF14-71D3-11ED-8D49-9D4DF4D3414C.xml"
+
+#obtenemos el objeto xml
+
+tree = ET.parse(CFDI_TASA_0)
+
+#obtenemos el root del XML 
 root = tree.getroot()
 
-tree_CFDI = ET.parse("./CFDI/A219EF14-71D3-11ED-8D49-9D4DF4D3414C.xml")
-root_CFDI = tree_CFDI.getroot()
-
-class reed_xml :
-    def __init__(self, document) -> None:
-        pass
+# imprimimos el 
+print(f"rooot : {root.tag}")
 
 
-print(root_CFDI.tag)
+print(root.tag)
     # for child in root_CFDI:
     #     print(child.tag, child.attrib)
 # for child in root: 
 #     print(child.attrib)
 
-print(root[0][1].text)
 
 # me permite obtener los datos de emisor del CFDI 
-print(root_CFDI[0].attrib)
+print("0 ", root[0].attrib)
 
 # me permite obtener los datos del receptor del CFDI 
-print(root_CFDI[1].attrib)
+print(root[1].attrib)
 
 # me permite obtener los datos de compra del producto
-print(root_CFDI[2][0].attrib)
+print(root[2][0].attrib)
 
 # me permite obtener los datos de los impuestos trasladados 
-print(root_CFDI[2][0][0][0][0].attrib["TasaOCuota"])
+print(root[2][0][0][0][0].attrib["TasaOCuota"])
+
+conceptos = root[2]
+print(conceptos.attrib)
+
+print("products")
+for concepto in conceptos:
+    print(f"""
+    f{concepto.attrib}
+    f{concepto[0][0][0].attrib}
+    _____________________________________________________________
+    """)
